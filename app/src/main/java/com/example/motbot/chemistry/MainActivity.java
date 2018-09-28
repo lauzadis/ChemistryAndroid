a87eb1d51ef7dabd2ebba7b986cb4820af2ba7bc
mataslauzadis <30608308+mataslauzadis@users.noreply.github.com>
2018-09-27 22:31:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/motbot/chemistry/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.motbot.chemistry;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button molsToGrams = (Button)findViewById(R.id.molsToGrams);
+        Button gramsToMols = (Button) findViewById(R.id.gramsToMols);
+
+        molsToGrams.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent molsToGramsSwitcher = new Intent(getApplicationContext(), molsToGrams.class);
+                startActivity(molsToGramsSwitcher);
+            }
+        });
+
+        gramsToMols.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gramsToMolsSwitcher = new Intent(getApplicationContext(), gramsToMols.class);
+                startActivity(gramsToMolsSwitcher);
+            }
+        });
+
+
+    }
+}
